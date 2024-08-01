a4ba45f7316df9c9ca145bb72dc5ff0e913710e1
vardhangkwm3 <vardhangupta789@gmail.com>
2024-08-01 13:55:43 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [students, setStudents] = useState([])
+
+  useEffect(()=>{
+    async function getAllStud(){
+      try {
+        const students = await axios.get("http://127.0.0.1:8000/api/student/")
+        console.log(students.data)
+        setStudents(students.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getAllStud()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Connectd React JS to Django</h1>
+      {
+        students.map((student, i)=>{
+          return(
+            <h2 key={i}>{student.studname} || {student.email}</h2>
+          )
+        })
+      }
     </div>
   );
 }
